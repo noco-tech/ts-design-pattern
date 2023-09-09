3e5ef12bc4402e088e91ad810bfd1bdcceba2a77
sadainoaraisan <73414778+sadainoaraisan@users.noreply.github.com>
2023-09-09 11:50:51 +0900

    firstcommit

---
+++ 01-setup/setup.ts
@@ -0,0 +1,7 @@
+export {} //名前衝突を回避
+
+function run() {
+    console.log('SETUP!');
+}
+
+run();
---
+++ 02-uml/classDiagramPractice/practice1.ts
@@ -0,0 +1,36 @@
+// // クラス図 演習1
+// class Employee {
+//     private id: number;
+//     private name: string;
+//     private salary: number;
+
+//     work() {
+//         console.log('働きます');
+//     }
+
+//     protected getSalary():number {
+//         return this.salary;
+//     }
+
+//     protected setSalary(salary: number) {
+//         this.salary = salary;
+//     }
+// }
+
+class Employee {
+    private id: number;
+    private name: string;
+    private salary: number;
+
+    work() {
+        console.log('働きます');
+    }
+
+    protected getSalary(): number {
+        return this.salary;
+    }
+
+    protected setSalary(salary: number) {
+        this.salary = salary;
+    }
+}
